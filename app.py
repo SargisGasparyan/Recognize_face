66698c51734800b1e25e7692e432a677e3efaba5
Sargis <sargisgasparyan081991@gmail.com>
2021-05-08 13:55:42 +0400

    first

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
 import os
 from flask import Flask, render_template, request, jsonify
 from compare_faces import compare
-import requests
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 app.config["IMAGE_UPLOADS"] = ""
 CORS(app)
-url = 'https://crm-emotion-rec.herokuapp.com/emotion_recognizer'
-image2 = 'img.jpg'
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/face_recognizer', methods = ['GET', 'POST'])
+def recocnize_faces():
+   if request.method == 'POST':
+       image1 = request.files['image1']
+       image2 = request.files['image2']
+       image1.save(os.path.join(app.config["IMAGE_UPLOADS"], image1.filename))
+       image2.save(os.path.join(app.config["IMAGE_UPLOADS"], image2.filename))
+       response = compare(image1, image2)
+       image = {'image': open(image2.filename, 'rb')}
+       if response == 'True':
+           return jsonify(
+               ImageProfile=image1.filename,
+               ImageWebcam=image2.filename,
+               response=response)
+       else:
+           return jsonify(
+               ImageProfile=image1.filename,
+               ImageWebcam=image2.filename,
+               response=response)
+
 
 
 
@@ -20,7 +38,7 @@ def index():
 def face_or_other():
    if request.method == 'POST':
        image1 = request.files['image1']
-       image1.save(os.path.join(app.config["IMAGE_UPLOADS"], image1.filename))
+       image2 = request.files['image2']
        response = compare(image1, image2)
        if ((response == 'True') or (response == 'False') ):
            return jsonify(
@@ -28,5 +46,6 @@ def face_or_other():
        else:
            return jsonify(
                response='no_face')
+
 if __name__ == '__main__':
     app.run()
